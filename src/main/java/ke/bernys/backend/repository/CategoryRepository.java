ca2bb46157172a273a2fe23480beb7799dc56bb8
Brian-Weloba <bweloba@gmail.com>
2022-08-03 20:16:27 +0300

    Add product and category repositories

---
+++ src/main/java/ke/bernys/backend/repository/CategoryRepository.java
@@ -0,0 +1,149 @@
+package ke.bernys.backend.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import ke.bernys.backend.models.Category;
+
+@Repository
+public class CategoryRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    /**
+     * This method creates a category in the database.
+     * 
+     * @param category The category to be created.
+     * @return Category The category that was created.
+     * @throws IllegalArgumentException If the category is null.
+     *
+     */
+    @Transactional
+    public void saveCategory(Category category) throws IllegalArgumentException {
+        // check if all the fields are filled
+        if (category.getName() == null || category.getDescription() == null) {
+            throw new IllegalArgumentException("The name and description of the category must be filled");
+        }
+        entityManager.persist(category);
+    }
+
+    /**
+     * This method gets a category from the database.
+     * 
+     * @param id The id of the category to be retrieved.
+     * @return Category The category that was retrieved.
+     * @throws IllegalArgumentException If the id is null.
+     * @throws EntityNotFoundException  If the category does not exist.
+     */
+    @Transactional
+    public Category getCategory(Long id) throws IllegalArgumentException, EntityNotFoundException {
+        // check if the id is null
+        if (id == null) {
+            throw new IllegalArgumentException("The id of the category must be filled");
+        }
+        // if category does not exist
+        else if (entityManager.find(Category.class, id) == null) {
+            throw new EntityNotFoundException("The category does not exist");
+        } else {
+            return entityManager.find(Category.class, id);
+        }
+    }
+
+    /**
+     * This method updates a category in the database.
+     * 
+     * @param category The category to be updated.
+     * @param id       The id of the category to be updated.
+     * @return Category The category that was updated.
+     * 
+     * @throws IllegalArgumentException If the category or id is null.
+     * @throws EntityNotFoundException  If the category does not exist.
+     *
+     */
+    @Transactional
+    public Category updateCategory(Category category, Long id)
+            throws IllegalArgumentException, EntityNotFoundException {
+        Category categoryToUpdate = entityManager.find(Category.class, id);
+        if (id == null) {
+            throw new IllegalArgumentException("The id of the category must be filled");
+        } else if (categoryToUpdate == null) {
+            throw new EntityNotFoundException("The category does not exist");
+        } else {
+            categoryToUpdate.setName(category.getName());
+            categoryToUpdate.setDescription(category.getDescription());
+            entityManager.merge(categoryToUpdate);
+            return categoryToUpdate;
+        }
+    }
+
+    /**
+     * This method deletes a category from the database.
+     * 
+     * @param id The id of the category to be deleted.
+     * @return Category The category that was deleted.
+     */
+    @Transactional
+    public void deleteCategory(Long id) {
+        Category category = getCategory(id);
+        entityManager.remove(category);
+    }
+
+    /**
+     * This method deletes all categories from the database.
+     * 
+     */
+    @Transactional
+    public void deleteAllCategories() {
+        entityManager.createQuery("DELETE FROM Category").executeUpdate();
+    }
+
+    /**
+     * This method gets all categories from the database.
+     * 
+     * @return List<Category> The list of categories.
+     */
+    @Transactional
+    public List<Category> getAllCategories() throws EntityNotFoundException {
+        List<Category> allCategories = entityManager.createQuery("SELECT c FROM Category c", Category.class)
+                .getResultList();
+        if (allCategories.isEmpty()) {
+            throw new EntityNotFoundException("There are no categories in the database");
+        } else {
+            return allCategories;
+        }
+    }
+
+    /**
+     * This method gets all categories from the database.
+     * 
+     * @param name
+     * @return List<Category> The list of categories.
+     * @throws IllegalArgumentException If the name is null.
+     * @throws EntityNotFoundException If the category does not exist.
+     *
+     */
+    @Transactional
+    public List<Category> findCategoryByName(String name) throws IllegalArgumentException, EntityNotFoundException {
+        if (name == null) {
+            throw new IllegalArgumentException("The name of the category must be filled");
+        } else {
+            List<Category> category = entityManager
+                    .createQuery("SELECT c FROM Category c WHERE c.name = :name", Category.class)
+                    .setParameter("name", name).getResultList();
+            if (category.isEmpty()) {
+                throw new EntityNotFoundException("The category does not exist");
+            } else {
+                return category;
+            }
+
+        }
+
+    }
+}
---
+++ src/main/java/ke/bernys/backend/repository/ProductRepository.java
@@ -0,0 +1,111 @@
+package ke.bernys.backend.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import ke.bernys.backend.models.Product;
+
+@Repository
+public class ProductRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    /**
+     * This method creates a product in the database.
+     * 
+     * @param product The product to be created.
+     * @return Product The product that was created.
+     * @throws IllegalArgumentException If the product is null.
+     *
+     */
+    @Transactional
+    public void saveProduct(Product product) throws IllegalArgumentException {
+        // check if all the fields are filled
+        if (product.getName() == null || product.getDescription() == null) {
+            throw new IllegalArgumentException("The name and description of the product must be filled");
+        }
+        entityManager.persist(product);
+    }
+
+    /**
+     * This method gets a product from the database.
+     * 
+     * @param id The id of the product to be retrieved.
+     * @return Product The product that was retrieved.
+     * @throws IllegalArgumentException If the id is null.
+     * @throws EntityNotFoundException  If the product does not exist.
+     */
+    @Transactional
+    public Product getProduct(Long id) throws IllegalArgumentException, EntityNotFoundException {
+        // check if the id is null
+        if (id == null) {
+            throw new IllegalArgumentException("The id of the product must be filled");
+        }
+        // if product does not exist
+        else if (entityManager.find(Product.class, id) == null) {
+            throw new EntityNotFoundException("The product does not exist");
+        } else {
+            return entityManager.find(Product.class, id);
+        }
+    }
+
+    /**
+     * This method updates a product in the database.
+     * 
+     * @param product The product to be updated.
+     * @return Product The product that was updated.
+     * @throws IllegalArgumentException If the product is null.
+     * @throws EntityNotFoundException  If the product does not exist.
+     */
+    @Transactional
+    public Product updateProduct(Product product) throws IllegalArgumentException, EntityNotFoundException {
+        // check if the product is null
+        if (product == null) {
+            throw new IllegalArgumentException("The product must be filled");
+        }
+        // check if the product exists
+        else if (entityManager.find(Product.class, product.getId()) == null) {
+            throw new EntityNotFoundException("The product does not exist");
+        } else {
+            entityManager.merge(product);
+            return product;
+        }
+    }
+
+    /**
+     * This method deletes a product from the database.
+     * 
+     * @param id The id of the product to be deleted.
+     * @throws IllegalArgumentException If the id is null.
+     * @throws EntityNotFoundException  If the product does not exist.
+     */
+    @Transactional
+    public void deleteProduct(Long id) throws IllegalArgumentException, EntityNotFoundException {
+        // check if the id is null
+        if (id == null) {
+            throw new IllegalArgumentException("The id of the product must be filled");
+        }
+        // check if the product exists
+        else if (entityManager.find(Product.class, id) == null) {
+            throw new EntityNotFoundException("The product does not exist");
+        } else {
+            entityManager.remove(entityManager.find(Product.class, id));
+        }
+    }
+
+    @Transactional
+    public void deleteAllProducts() {
+        entityManager.createQuery("DELETE FROM Product").executeUpdate();
+    }
+
+    @Transactional
+    public List<Product> getAllProducts() {
+        return entityManager.createQuery("SELECT p FROM Product p", Product.class).getResultList();
+    }
+}
