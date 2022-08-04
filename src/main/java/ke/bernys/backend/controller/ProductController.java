fbc0878011513cb1d725e9884227612bb6845514
Brian-Weloba <bweloba@gmail.com>
2022-08-04 09:32:30 +0300

    Add swagger UI

--- src/main/java/ke/bernys/backend/controller/ProductController.java
+++ src/main/java/ke/bernys/backend/controller/ProductController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ke.bernys.backend.models.Product;
 import ke.bernys.backend.service.ProductService;
 
 @RestController
+@RequestMapping("/api/v1/")
 public class ProductController {
 
     @Autowired
